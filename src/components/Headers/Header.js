77e8e3ab6375309bcc9e24d01a939819898a3612
murat aranci <murataranciii@gmail.com>
2022-06-01 18:05:33 +0300

    Header complated

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Header.css';
 import {FaSearch} from 'react-icons/fa'
 import {AiOutlineShoppingCart} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
 
 const Header = () => {
   return (
@@ -9,6 +10,7 @@ const Header = () => {
         <img className='starbucksLogo' src='https://marka-logo.com/wp-content/uploads/2020/09/Starbucks-Logo.png'></img>
         <a className='menu' href=''>Menü</a>
         <input className='arama' placeholder='Aramadığınız ürünü yazınız..'></input><button className='searchButton'><FaSearch/></button>
+        <AiOutlineUser className='user'/>
         <a className='giris' href=''>Giriş yap</a>
         <a className='kayıt' href=''>Kayıt ol</a>
         <button className='Shoppingcart'><AiOutlineShoppingCart/></button>
