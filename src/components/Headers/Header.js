93a34becdb5fdc103a27864cf97989d8cb5ca7b2
murat aranci <murataranciii@gmail.com>
2022-06-01 15:54:46 +0300

    Header complated

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import './Header.css';
 import {FaSearch} from 'react-icons/fa'
-
+import {AiOutlineShoppingCart} from 'react-icons/ai'
 
 const Header = () => {
   return (
     <div className='navbar'>
         <img className='starbucksLogo' src='https://marka-logo.com/wp-content/uploads/2020/09/Starbucks-Logo.png'></img>
-        <a className='menu' href=''>Menu</a>
-        <a className='arama' href=''><FaSearch/></a>
+        <a className='menu' href=''>Menü</a>
+        <input className='arama' placeholder='Aramadığınız ürünü yazınız..'></input><button className='searchButton'><FaSearch/></button>
+        <a className='giris' href=''>Giriş yap</a>
+        <a className='kayıt' href=''>Kayıt ol</a>
+        <button className='Shoppingcart'><AiOutlineShoppingCart/></button>
+
     </div>
   )
 }
 
-export default Header
+export default Header 
